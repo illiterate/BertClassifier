43c05ac16c44fdedb1f1293b00cef51594533731
Donghuan HE <victor-he@qq.com>
2023-05-22 23:47:12 +0800

    fix bugs of BertLstmClassifier

--- model.py
+++ model.py
@@ -30,15 +30,16 @@ class BertClassifier(nn.Module):
 
 # Bert+BiLSTM，用法与BertClassifier一样，可直接在train里面调用
 class BertLstmClassifier(nn.Module):
-    def __init__(self, bert_config):
+    def __init__(self, bert_config, num_labels):
         super().__init__()
         self.bert = BertModel(config=bert_config)
         self.lstm = nn.LSTM(input_size=bert_config.hidden_size, hidden_size=bert_config.hidden_size, num_layers=2, batch_first=True, bidirectional=True)
-        self.classifier = nn.Linear(bert_config.hidden_size*2, bert_config.num_labels)  # 双向LSTM 需要乘以2
+        self.classifier = nn.Linear(bert_config.hidden_size*2, num_labels)  # 双向LSTM 需要乘以2
         self.softmax = nn.Softmax(dim=1)
 
     def forward(self, input_ids, attention_mask, token_type_ids):
-        output, pooled = self.bert(input_ids=input_ids, attention_mask=attention_mask, token_type_ids=token_type_ids)
-        out, _ = self.lstm(output)
+        outputs = self.bert(input_ids=input_ids, attention_mask=attention_mask, token_type_ids=token_type_ids)
+        last_hidden_state = outputs.last_hidden_state
+        out, _ = self.lstm(last_hidden_state)
         logits = self.classifier(out[:, -1, :]) # 取最后时刻的输出
         return self.softmax(logits)
