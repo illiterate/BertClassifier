fix softmax bugs

dataset.py
@@ -5,19 +5,16 @@
 # @Time: 2021/12/09 11:01:32
 # @Description:
 
-import torch
 import numpy as np
-import torch.nn as nn
-from torch.utils.data import Dataset
-from transformers import BertTokenizer
 from tqdm import tqdm
+from torch.utils.data import Dataset
 
 class CNewsDataset(Dataset):
-    def __init__(self, filename):
+    def __init__(self, filename, tokenizer):
         # 数据集初始化
         self.labels = ['体育', '娱乐', '家居', '房产', '教育', '时尚', '时政', '游戏', '科技', '财经']
         self.labels_id = list(range(len(self.labels)))
-        self.tokenizer = BertTokenizer.from_pretrained('bert-base-chinese')
+        self.tokenizer = tokenizer
         self.input_ids = []
         self.token_type_ids = []
         self.attention_mask = []

model.py
@@ -5,9 +5,8 @@
 # @Time: 2020/10/10 17:12:56
 # @Description:
 
-import torch
 import torch.nn as nn
-from transformers import  BertModel
+from transformers import BertModel
 
 # Bert
 class BertClassifier(nn.Module):
@@ -26,7 +25,7 @@ class BertClassifier(nn.Module):
         # 分类
         logits = self.classifier(pooled)
         # 返回softmax后结果
-        return torch.softmax(logits, dim=1)
+        return logits
 
 # Bert+BiLSTM，用法与BertClassifier一样，可直接在train里面调用
 class BertLstmClassifier(nn.Module):
@@ -35,11 +34,10 @@ class BertLstmClassifier(nn.Module):
         self.bert = BertModel(config=bert_config)
         self.lstm = nn.LSTM(input_size=bert_config.hidden_size, hidden_size=bert_config.hidden_size, num_layers=2, batch_first=True, bidirectional=True)
         self.classifier = nn.Linear(bert_config.hidden_size*2, num_labels)  # 双向LSTM 需要乘以2
-        self.softmax = nn.Softmax(dim=1)
 
     def forward(self, input_ids, attention_mask, token_type_ids):
         outputs = self.bert(input_ids=input_ids, attention_mask=attention_mask, token_type_ids=token_type_ids)
         last_hidden_state = outputs.last_hidden_state
         out, _ = self.lstm(last_hidden_state)
         logits = self.classifier(out[:, -1, :]) # 取最后时刻的输出
-        return self.softmax(logits)
+        return logits

train.py
@@ -19,15 +19,18 @@ from sklearn import metrics
 def main():
 
     # 参数设置
+    model_path = r'D:/Workspace/Python/pretrained-models/bert-base-chinese/'
+    data_path = r'D:/Workspace/Python/cnews/'
     batch_size = 4
     device = 'cuda' if torch.cuda.is_available() else 'cpu'
     epochs = 10
     learning_rate = 5e-6    #Learning Rate不宜太大
+    tokenizer = BertTokenizer.from_pretrained(model_path)
 
     # 获取到dataset
-    train_dataset = CNewsDataset('data/cnews/cnews.train.txt')
-    valid_dataset = CNewsDataset('data/cnews/cnews.val.txt')
-    #test_dataset = CNewsDataset('data/cnews/cnews.test.txt')
+    train_dataset = CNewsDataset(data_path + 'cnews.train.txt', tokenizer)
+    valid_dataset = CNewsDataset(data_path + 'cnews.val.txt', tokenizer)
+    #test_dataset = CNewsDataset(data_path + 'cnews.test.txt', tokenizer)
 
 
     # 生成Batch
@@ -36,7 +39,7 @@ def main():
     #test_dataloader = DataLoader(test_dataset, batch_size=batch_size, shuffle=False)
 
     # 读取BERT的配置文件
-    bert_config = BertConfig.from_pretrained('bert-base-chinese')
+    bert_config = BertConfig.from_pretrained(model_path)
     num_labels = len(train_dataset.labels)
 
     # 初始化模型